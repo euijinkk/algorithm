242e62481e526089f33e18689728ce3c228398ad
euijinkk <gytks4@naver.com>
2023-02-05 17:36:30 +0900

    scaffolding scripts 추가

---
+++ scripts/scaffolding.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+filename=$1
+
+if [ "$1" = "" ]; then
+  echo "알고리즘 문제 이름을 적어주세요 : "
+  read filename
+fi
+
+touch "$filename.js"
+echo "/**" >>"$filename".js
+echo " * 풀이 시간 : 15:03" >>"$filename".js
+echo " *" >>"$filename".js
+echo " * 풀이 과정 :" >>"$filename".js
+echo " *" >>"$filename".js
+echo " * 풀이 참고 및 Learning :" >>"$filename".js
+echo " */" >>"$filename".js
