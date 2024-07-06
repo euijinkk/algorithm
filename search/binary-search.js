a2082dc466bde32862fe5d4499420ea2a22d0c9f
gytks4 <gytks4@naver.com>
2024-07-06 16:03:15 +0900

    feat: binary search while

--- search/binary-search.js
+++ search/binary-search.js
@@ -1,4 +1,4 @@
-function findByBinarySearch(arr, target) {
+function findByBinarySearchResursive(arr, target) {
   let result = undefined;
   function recursive(currentArr) {
     if (currentArr.length === 0) {
@@ -21,4 +21,23 @@ function findByBinarySearch(arr, target) {
   recursive(arr);
 
   return result;
+}
+
+function findByBinarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return undefined;
 }
