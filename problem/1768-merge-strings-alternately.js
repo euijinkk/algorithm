9ed2c228484d0bbfb11c056d874d4923c2ec1765
gytks4 <gytks4@naver.com>
2024-09-02 22:13:36 +0900

    add comment

--- problem/1768-merge-strings-alternately.js
+++ problem/1768-merge-strings-alternately.js
@@ -1,8 +1,13 @@
 function mergeAlternately(word1, word2) {
-  const maxLength = Math.max(word1.length, word2.length);
+  // Initialize an empty string to store the result
   const res = "";
 
+  // Determine the maximum length between the two words.
+  const maxLength = Math.max(word1.length, word2.length);
+
+  // Loop through each index up to the maximum length
   for (let i = 0; i < maxLength; i++) {
+    // If the current index is within the bounds of word1, add the character to the result
     if (i < word1.length) {
       res = res + word1[i];
     }
@@ -11,5 +16,6 @@ function mergeAlternately(word1, word2) {
     }
   }
 
+  // Return the final merged string
   return res;
 }
