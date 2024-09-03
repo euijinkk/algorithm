10825dad48ca9f7cfef3ca2f2c3574424bb0f68d
gytks4 <gytks4@naver.com>
2024-09-03 16:05:30 +0900

    commentsi

--- problem/1071-greatest-common-divisor-of-strings.js
+++ problem/1071-greatest-common-divisor-of-strings.js
@@ -1,11 +1,18 @@
+// I'll solve the problem of finding the greatest common divisor of two strings
 function gcdOfStrings(str1, str2) {
+  // First, get divisors of str1
   const stringDivisors1 = getStringDivisors(str1);
+  // Then, get divisors of str2
   const stringDivisors2 = getStringDivisors(str2);
 
+  // Finally, find the common divisors of str1 and str2
   const commonArr = findCommon(stringDivisors1, stringDivisors2);
+
+  // Then, get get greatest one.
   return getMaxLengthElement(commonArr) ?? "";
 }
 
+// get the element with maximum length in the array
 function getMaxLengthElement(arr) {
   return arr.reduce((acc, curr) => {
     if (curr.length > acc.length) {
@@ -15,10 +22,13 @@ function getMaxLengthElement(arr) {
   }, arr[0]);
 }
 
+// find common elements between two arrays
 function findCommon(arr1, arr2) {
   return arr1.filter((e) => arr2.includes(e));
 }
 
+// This function gets the string divisors of a string
+// It takes a string as a parameter and returns an array of strings
 function getStringDivisors(str) {
   const res = [];
   const divisorsOfNumber = getNumberDivisors(str.length);
@@ -32,6 +42,8 @@ function getStringDivisors(str) {
   return res;
 }
 
+// This function gets the divisors of a number
+// It takes a number as a parameter and returns an array of numbers
 function getNumberDivisors(num) {
   const res = new Set();
   for (let i = 1; i <= Math.sqrt(num); i++) {
