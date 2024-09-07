8816273cfd374dd8d4e2278f7c2906fafd6fae15
gytks4 <gytks4@naver.com>
2024-09-07 17:18:21 +0900

    comment

--- problem/283-Move-Zeroes.js
+++ problem/283-Move-Zeroes.js
@@ -2,18 +2,24 @@
  * O(2n)
  */
 function moveZeroes(nums) {
+  // Declare the nonZeroIndexes variable as an empty array
   const nonZeroIndexes = [];
 
+  // Iterate through the nums array
   for (num of nums) {
+    // If num is not 0, push it to nonZeroIndexes
     if (num !== 0) {
       nonZeroIndexes.push(num);
     }
   }
 
+  // Loop through the length of nums
   for (let i = 0; i < nums.length; i++) {
+    // If i is less than the length of nonZeroIndexes, assign the value at index i to nums
     if (i < nonZeroIndexes.length) {
       nums[i] = nonZeroIndexes[i];
     } else {
+      // Otherwise, fill it with 0
       nums[i] = 0;
     }
   }
@@ -25,7 +31,8 @@ function moveZeroes(nums) {
  * O(n)
  */
 function moveZeroes2(nums) {
-  let i = 0; // Pointer to track the position of the next non-zero element
+  // set i as pointer to track the position of the next non-zero element
+  let i = 0;
 
   for (let j = 0; j < nums.length; j++) {
     if (nums[j] !== 0) {
