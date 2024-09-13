9404d6b1ddfc0cc45918efb3c04d96b8a0fe9968
gytks4 <gytks4@naver.com>
2024-09-13 22:16:27 +0900

    scaffolding

--- scripts/scaffolding.sh
+++ scripts/scaffolding.sh
@@ -7,11 +7,14 @@ if [ "$1" = "" ]; then
   read filename
 fi
 
-touch "$filename.js"
-echo "/**" >>"$filename".js
-echo " * 풀이 시간 : " >>"$filename".js
-echo " *" >>"$filename".js
-echo " * 풀이 과정 :" >>"$filename".js
-echo " *" >>"$filename".js
-echo " * 풀이 참고 및 Learning :" >>"$filename".js
-echo " */" >>"$filename".js
+filepath="./problem/$filename.js"
+
+# 파일 생성 및 내용 추가
+touch "$filepath"
+{
+  echo "/**"
+  echo " * 1. 설명하면서 문제 풀기 "
+  echo " * 2. 알고리즘 전략 분석 (시간복잡도, 공간복잡도, 자료구조, 방법론)"
+  echo " * 3. 알고리즘 개선하기"
+  echo " */"
+} >>"$filepath"
